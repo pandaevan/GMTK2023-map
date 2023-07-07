f69afac220684051b3afbc9950bcfccdb233c97e
Shark1ebyt3 <87788118+Shark1ebyt3@users.noreply.github.com>
2023-07-07 13:53:12 -0700

    Movement v1
    Cringe, nightmare, bad movement. Moves like Dom Toretto drives.

--- Rougeknight/Assets/Scripts/KnightMovement.cs
+++ Rougeknight/Assets/Scripts/KnightMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,14 @@ public class NewBehaviourScript : MonoBehaviour
 
     [SerializeField] Rigidbody2D rb;
 
-    [Header("Movement")] [SerializeField] float speed = 1f;
+    [Header("Movement")] [SerializeField] float speed;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
+    
+    
     void FixedUpdate()
     {
         float htrans = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
@@ -22,26 +24,25 @@ public class NewBehaviourScript : MonoBehaviour
 
         switch (htrans) 
         {
-            case < 0:
-                
-                break;
             case > 0:
-
+                rb.AddRelativeForce(Vector2.right * speed);
+                break;
+            case < 0:
+                rb.AddRelativeForce(Vector2.left * speed);
                 break;
         }
         switch (vtrans) 
         {
-            case < 0:
-
+            case > 0:
+                rb.AddRelativeForce(Vector2.up * speed);
                 break;
 
-            case > 0:
-
+            case < 0:
+                rb.AddRelativeForce(Vector2.down * speed);
                 break;
         }
-
-        transform.Translate(htrans, vtrans, 0);
     } 
+    
 
     /*
     // Update is called every fixed framerate frame?
