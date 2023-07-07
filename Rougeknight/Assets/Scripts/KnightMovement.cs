7bdcfe877801243e68739fefe6cc877645a6db46
Shark1ebyt3 <87788118+Shark1ebyt3@users.noreply.github.com>
2023-07-07 16:23:59 -0700

    Movement v4
    New movement script just dropped

--- Rougeknight/Assets/Scripts/KnightMovement.cs
+++ Rougeknight/Assets/Scripts/KnightMovement.cs
@@ -3,32 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    Vector2 moveinput;
+    private Vector2 direction;
     [SerializeField] Rigidbody2D rb;
 
-    float hmove;
-    float vmove;
+    [Header("Movement")][SerializeField] float speed;
 
-    [Header("Movement")] [SerializeField] float speed;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    //Called every frame
     private void Update()
     {
-        moveinput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        moveinput = Vector2.ClampMagnitude(moveinput * speed, speed);
+        InputsProcess();
     }
 
-
+    //Called a set amount per an update
     void FixedUpdate()
     {
-        transform.Translate(moveinput.x * Time.fixedDeltaTime, moveinput.y * Time.fixedDeltaTime, 0);
+        Movement();
+    }
+
+    void InputsProcess() 
+    {
+        float Xmove = Input.GetAxisRaw("Horizontal");
+        float Ymove = Input.GetAxisRaw("Vertical");
+
+        direction = new Vector2(Xmove, Ymove).normalized;
+    }
+    void Movement() 
+    {
+        rb.velocity = new Vector2(direction.x * speed, direction.y * speed);
     }
 }
