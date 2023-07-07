2707255821b41016bd8bc5e3a8da5c18c68f1e4f
Shark1ebyt3 <87788118+Shark1ebyt3@users.noreply.github.com>
2023-07-07 14:14:11 -0700

    Movement v2
    Maybe simpler is better? Moves like a GTA vehicle.

--- Rougeknight/Assets/Scripts/KnightMovement.cs
+++ Rougeknight/Assets/Scripts/KnightMovement.cs
@@ -15,42 +15,12 @@ public class NewBehaviourScript : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    
-    
-    void FixedUpdate()
-    {
-        float htrans = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        float vtrans = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
-        switch (htrans) 
-        {
-            case > 0:
-                rb.AddRelativeForce(Vector2.right * speed);
-                break;
-            case < 0:
-                rb.AddRelativeForce(Vector2.left * speed);
-                break;
-        }
-        switch (vtrans) 
-        {
-            case > 0:
-                rb.AddRelativeForce(Vector2.up * speed);
-                break;
-
-            case < 0:
-                rb.AddRelativeForce(Vector2.down * speed);
-                break;
-        }
-    } 
-    
-
-    /*
-    // Update is called every fixed framerate frame?
     void FixedUpdate()
     {
         float htrans = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         float vtrans = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
         transform.Translate(htrans, vtrans, 0);
-    } */
+    } 
 }
