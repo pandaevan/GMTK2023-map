f5028c9a3cf185b3b478094763931c4420f45915
pandaevan <evantwilds@gmail.com>
2023-07-09 01:29:38 -0700

    f

--- Rougeknight/Assets/Scripts/HealthManager.cs
+++ Rougeknight/Assets/Scripts/HealthManager.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
+    statsdegrade std;
     [SerializeField] public int maxHealth = 100;
-    [SerializeField] public int currentHealth = 50;
+    [SerializeField] public int currentHealth;
 
     [SerializeField] Slider slider;
 
@@ -42,33 +43,6 @@ public class HealthManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     { 
-
-    }
-
-    //Increases health by set amount, meant to be accessed by item
-    public void IncreaseHealth(int modifier)
-    {
-        if (!(currentHealth + modifier > maxHealth))
-        {
-            currentHealth += modifier;
-        } //issue: will add no health if modifier brings health up to maxhealth
-    }
-
-    //Increases max health by set amount, meant to be accessed by item
-    public void IncreaseMaxHealth(int modifier)
-    {
-        if (!(maxHealth + modifier > 100))
-        {
-            currentHealth += modifier;
-        }
-    }
-
-    public void TakeDamage(int damage)
-    {
-        if (!audioSource.isPlaying)
-        {
-            audioSource.PlayOneShot(hurt);
-        }
-        currentHealth -= damage;
+    currentHealth = std.BaseHP;
     }
 }
